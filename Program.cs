24dc5fdf575df79b9aa37a200f433038104a63bd
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-04-22 02:30:41 -0300

    Refactoring and database config.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-using TheWitcherBestiary.Models;
+using Bestiary.Models;
+using Bestiary.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.Configure<TheWitcherBestiaryDatabaseSettings>(
-    builder.Configuration.GetSection("TheWitcherBestiary"));
+builder.Services.Configure<BestiaryDatabaseSettings>(
+    builder.Configuration.GetSection("Bestiary"));
+
+builder.Services.AddSingleton<BestiaryServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
