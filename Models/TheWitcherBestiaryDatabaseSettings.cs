430129f2df628dda9ffc57cd515f9ca6a8617c7d
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-04-21 22:15:03 -0300

    Database configuration.

---
+++ Models/TheWitcherBestiaryDatabaseSettings.cs
@@ -0,0 +1,10 @@
+namespace TheWitcherBestiary.Models;
+
+public class TheWitcherBestiaryDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string TheWitcherBestiary { get; set; } = null!;
+
+    public string BestiaryCollectionName { get; set; } = null!;
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using TheWitcherBestiary.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<TheWitcherBestiaryDatabaseSettings>(
+    builder.Configuration.GetSection("TheWitcherBestiary"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
