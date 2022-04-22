0595f4c843301a147c0ba45783760d9be716d2b8
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-04-22 02:30:00 -0300

    Refactoring

--- Models/Bestiary.cs
+++ Models/Bestiary.cs
@@ -1,28 +1,27 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace TheWitcherBestiary.Models
+namespace Bestiary.Models;
+
+ public class Beast
 {
-    public class Bestiary
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+     [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
-        public string? Name { get; set; }
+    public string? Name { get; set; }
 
-        public string? Description { get; set; }
+    public string? Description { get; set; }
 
-        public string? Category { get; set; }
+    public string? Category { get; set; }
 
-        public string? Variations { get; set; }
+    public string? Variations { get; set; }
 
-        public string? Occurrences { get; set; }
+    public string? Occurrences { get; set; }
 
-        public string? Vulnerable { get; set; }
+    public string? Vulnerable { get; set; }
 
-        public string? Immunity { get; set; }
+    public string? Immunity { get; set; }
 
-        public string? Loot { get; set; }
-    }
+    public string? Loot { get; set; }
 }
