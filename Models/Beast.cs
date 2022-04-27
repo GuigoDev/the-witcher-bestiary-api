16f155c2a6ad1166aedd872b954f6a8f570d9c0c
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-04-27 03:30:29 -0300

    model refactoring

--- Models/Beast.cs
+++ Models/Beast.cs
@@ -1,22 +1,31 @@
+using System.ComponentModel.DataAnnotations;
 namespace BestiaryApi.Models;
 
 public class Beast
 {
     public int Id { get; set; }
 
+    [Required]
     public string? Name { get; set; }
 
+    [Required]
     public string? Description { get; set; }
 
+    [Required]
     public string? Category { get; set; }
 
+    [Required]
     public string? Variations { get; set; }
 
+    [Required]
     public string? Occurrences { get; set; }
 
+    [Required]
     public string? Vulnerable { get; set; }
 
+    [Required]
     public string? Immunity { get; set; }
 
+    [Required]
     public string? Loot { get; set; }
 }
