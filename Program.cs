148e10e848d835afe37675fd1bcea6694687e8a6
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-05-05 20:31:07 -0300

    Auth configurations.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using BestiaryApi.Services;
 using BestiaryApi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using BestiaryApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,23 @@ builder.Services.AddScoped<BestiaryServices>();
 builder.Services.AddScoped<UserServices>();
 
 builder.Services.AddControllers();
+
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
+builder.Services.AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +47,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
